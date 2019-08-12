9fdb7c992d62a816605701392b45d76d46b92f15
HancyWang <wanghouxun@126.com>
2019-08-12 13:55:04 +0800

    更新到1.2.2，打开logger功能

--- EAR_F030/HARDWARE/rtc.h
+++ EAR_F030/HARDWARE/rtc.h
@@ -2,7 +2,7 @@
 #define _RTC_H
 #include "stm32f0xx_rtc.h"
 
-#define RTC_DISABLE
+//#define RTC_DISABLE
 
 _Bool Set_RTC(uint8_t* pdata);
 void Get_DataTime(RTC_DateTypeDef* RTC_DateStructure,RTC_TimeTypeDef* RTC_TimeStructure);
